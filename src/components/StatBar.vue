7b58511c2cd051658d998c1bbcf5751923163085
Brandon Johnson <bmj1985@gmail.com>
2018-06-09 14:30:08 -0600

    added cutoff times

--- src/components/StatBar.vue
+++ src/components/StatBar.vue
@@ -6,7 +6,7 @@
           <v-card-text class="ma-1 pa-0 title text-xs-center">{{aidStation.name}}</v-card-text>
           <v-card-text class="pa-1 ma-1 text-xs-center">
               <div class="headline mb-2">{{aidStation.numberOfRunners}}</div>
-              <div>{{ aidStation.timeLeft }}</div>
+              <div v-if="aidStation.timeLeft != null">{{ aidStation.timeLeft }}</div>
           </v-card-text>
           <v-card-actions class="ma-1 pa-0">
             <v-btn left class="ma-0 pa-0" flat color="orange">Details</v-btn>
@@ -26,108 +26,94 @@ data () {
 return {
 aidStation: [{ name: 'Zone 1',
     numberOfRunners: this.$store.getters.zoneOne.length,
-    timeLeft: this.timeLeft('2018-06-12 14:19')
 },
 { name: 'Rasberry 1',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+     timeLeft: this.timeLeft('2018-08-03 08:00')
 },
 { name: 'Zone 2',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Antero',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-03 13:00')
 },
 { name: 'Zone 3',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'St Elmo One',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-03 15:30')
 },
 { name: 'Zone 4',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 {
     name: 'Cottonwood',
     numberOfRunners: 4,
-    timeLeft: '02:04:45'
+    timeLeft: this.timeLeft('2018-08-03 18:00')
 },
 { name: 'Zone 5',
-    numberOfRunners: 3,
-    timeLeft: '00:00:00'
+    numberOfRunners: 3
 },
 { name: 'St Elmo 2',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-03 20:30')
 },
 { name: 'Zone 6',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Hancock',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 01:30')
 },
 { name: 'Zone 7',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Lost Wonder',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 04:00')
 },
 { name: 'Zone 8',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Purgatory',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Zone 9',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Monarch',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 09:00')
 },
 { name: 'Zone 10',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
 },
 { name: 'Fooses',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 11:00')
 },
 { name: 'Zone 11',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Blanks',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 14:00')
 },
 { name: 'Zone 12',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Rasberry 2',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 16:45')
 },
 { name: 'Zone 13',
-    numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    numberOfRunners: 34
 },
 { name: 'Finish',
     numberOfRunners: 34,
-    timeLeft: '00:00:00'
+    timeLeft: this.timeLeft('2018-08-04 18:00')
 }]
 }
 },
